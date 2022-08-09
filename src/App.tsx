dbb507d118be35a2d73cc2854231252f468949b1
Celestino Langa <celestinolanga11@gmail.com>
2022-08-09 01:02:28 +0100

    add tailwindcss framework on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="min-h-screen bg-gray-800 flex flex-col center justify-center content-center text-white">
+        <img src={logo} className="h-72" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-cyan-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
